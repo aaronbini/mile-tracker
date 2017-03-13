221689566a10fc07272e5e08f17837a454e38499
Aaron Bini <bini.aaron@gmail.com>
2017-03-13 07:19:01 -0700

    changed apiUrl

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import auth from './auth';
 import http from './http';
 import './scss/main.scss';
 
-// app.constant('apiUrl', '/api');
-app.constant('apiUrl', 'http://localhost:3000/api');
+app.constant('apiUrl', '/api');
 
 app.config(http);
 app.config(routes);
